06f23df5b62c15a59859fdab5fe199913573dc21
Benjamin Straate <straateezy@BenjamiaatesMBP.attlocal.net>
2016-03-10 13:00:15 -0800

    added tweets and python files

---
+++ fletch.py
@@ -0,0 +1,105 @@
+#!/Users/straateezy/anaconda/bin/python
+
+import pickle
+import nltk
+from nltk.classify import NaiveBayesClassifier as nbc
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from textblob.classifiers import NaiveBayesClassifier as nbcblob
+from copy import deepcopy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import ne_chunk
+from nltk import pos_tag
+from nltk import word_tokenize
+from nltk import chunk
+
+'''
+Loads the pickle for my 4283 labelled tweets (Key below).  Also, converts this
+to another list, reversed_labelled_tweets_zip, which flips my (rank, tweet) tuples.
+
+From here, tweets are stripped from stopwords, non-alphas and lowercased, tokenized, part-of-speech tagged,
+stripped of proper nouns (NPP are replaced with 'blah') and finally the re-assembled tweets with corresponding
+hand-labellings to final_preprocessed_tweet_and_classification.
+
+Key:
+0 = Injury-related
+1 = Non-injury related incident effecting the game
+2 = Irrelevant tweets
+-----
+
+
+'''
+
+final_preprocessed_tweet_and_classification = []
+
+keep_chars = ['(', ')']
+gowords_lst = ['not play', 'out', 'out tonight', 'inactive', 'inactives', 'not return',
+              'questionable', 'game to game', 'injury', 'injured', 'sore', 'broken',
+              'hamstring', 'ACL']
+
+stopwords_lst = stopwords.words('english')
+stopwords_to_remove = ['out', 'isn', 'not']
+for stopword in stopwords_to_remove:
+    stopwords_lst.remove(stopword)
+
+def openpickle():
+    with open('/Users/straateezy/Notebooks/labelledzip.pkl', 'r') as picklefile1:
+        labelled_tweets_zip = pickle.load(picklefile1)
+        return labelled_tweets_zip
+
+
+def converttweets(labelled_tweets_zip):
+    reversed_labelled_tweets_zip = []
+    for item in labelled_tweets_zip:
+        reversed_labelled_tweets_zip.append((item[1], item[0]))
+    return reversed_labelled_tweets_zip
+    print 'returned reversed list'
+
+
+def preprocesstokenize(reversed_labelled_tweets_zip):
+    print len(reversed_labelled_tweets_zip)
+    for tweet, category in reversed_labelled_tweets_zip:
+        tokens = word_tokenize(tweet)
+        classification = category
+        tokens_tagged = pos_tag(tokens)
+        create_new_list(classification, tokens_tagged)
+
+def create_new_list(classification, tokens_tagged):
+    tweet_lst = []
+    for token in tokens_tagged:
+        word_tweeted_tagged_token = token[0]
+        grammatical_type_of_word = token[1]
+        if word_tweeted_tagged_token.lower() in stopwords_lst:
+            continue
+        elif grammatical_type_of_word == 'NNP':
+            tweet_lst.append('blah')
+        elif word_tweeted_tagged_token.isalpha() or word_tweeted_tagged_token in keep_chars:
+            tweet_lst.append(word_tweeted_tagged_token)
+    string_of_tweet_lst = ' '.join(tweet_lst).lower()
+    build_final_processed_list(string_of_tweet_lst, classification)
+
+def build_final_processed_list(string_of_tweet_lst, classification):
+    # FPTC = []
+    # FPTC.append((string_of_tweet_lst, classification))
+    final_preprocessed_tweet_and_classification.append((string_of_tweet_lst, classification))
+    if len(final_preprocessed_tweet_and_classification) % 50 == 0:
+        print 'finished processing tweet string: {}'.format(len(final_preprocessed_tweet_and_classification))
+
+def outpickle(final_preprocessed_tweet_and_classification):
+    with open('/Users/straateezy/atom/Fletcher/processed_labelled_list.pkl', 'w') as picklefile2:
+        pickle.dump(final_preprocessed_tweet_and_classification, picklefile2)
+        print 'dumping to pickle'
+    # with open('/Users/straateezy/atom/Fletcher/processed_labelled_list_backup.pkl', 'w') as picklefile3:
+    #     pickle.dump(backup, picklefile3)
+    #     print 'dumping to pickle backup'
+
+def main():
+    # print final_preprocessed_tweet_and_classification[:100]
+    labelled_tweets_zip = openpickle()
+    reversed_labelled_tweets_zip = converttweets(labelled_tweets_zip)
+    preprocesstokenize(reversed_labelled_tweets_zip)
+    outpickle(final_preprocessed_tweet_and_classification)
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,62 @@
+import pickle
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
+'''
+Takes the proc_labl_tweets we created in fletcher.py and turns them into two
+dictionaries with corresponding indicies as keys.  Then, converts these into
+two lists; checking for identical lengths.  Next, we build our MultinomialNB
+model and our vectorizer.  We fit the model and are ready to predict.
+
+'''
+
+
+def openpickle():
+    with open('/Users/straateezy/atom/Fletcher/processed_labelled_list.pkl', 'r') as picklefile:
+        proc_labl_tweets = pickle.load(picklefile)
+        return proc_labl_tweets
+
+# def separate_PLT(proc_labl_tweets):
+#     idx_tweet_dict = {}
+#     idx_category_dict = {}
+#     for idx, tweet, category in enumerate(proc_labl_tweets):
+#         idx_tweet_dict[idx] = tweet
+#         idx_category_dict[idx] = category
+#     return idx_tweet_dict, idx_category_dict
+#
+# def make_xy_train(idx_tweet_dict, idx_category_dict):
+#     x_train = []
+#     y_train = []
+#     if len(idx_tweet_dict) != len(idx_category_dict):
+#         print 'Something is wrong; tweet and cat dicts diff lengths'
+#     for x in range(len(idx_tweet_dict)):
+#         x_train.append(idx_tweet_dict[x])
+#         y_train.append(idx_category_dict[x])
+#     if len(x_train) != len(y_train):
+#         print 'Something is wrong; x and y train diff lengths'
+#     return x_train, y_train
+#
+# def build_MNBC_model(x_train, y_train):
+#     clf = MultinomialNB()
+#     vectorizer = TfidfVectorizer(ngram_range=(1,3))
+#     x_train_vec_trans = vectorizer.fit_transform(x_train)
+#     clf.fit(x_train_vec_trans, y_train)
+#     print clf.predict(vectorizer.transform(['injury tonight, john is out']))
+#
+#
+# def pickle_output_train(x_train, y_train):
+#     with open('x_train.pkl', 'w') as picklefileout:
+#         picklefileout.dump(x_train, picklefileout)
+#     with open('y_train.pkl', 'w') as picklefileout2:
+#         picklefileout2.dump(y_train, picklefileout2)
+
+def main():
+    proc_labl_tweets = openpickle()
+    print len(proc_labl_tweets)
+    # idx_tweet_dict, idx_category_dict = separate_PLT(proc_labl_tweets)
+    # x_train, y_train = make_xy_train(idx_tweet_dict, idx_category_dict)
+    # build_MNBC_model(x_train, y_train)
+
+
+if __name__ == '__main__':
+    main()
